0d52063d393af9a4eeef0d57655253ce85a580fc
komalchilke <komalchilke77@gmail.com>
2023-07-22 09:27:37 +0530

    basic program

---
+++ basic programs/setw.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main (){
+	int a =55, b=786 ,c=67888;
+	cout <<"a is "<<setw(5)<<a<<endl;
+	cout <<"b is "<<setw(5)<<b<<endl;
+	cout <<"c is "<<setw(5)<<c<<endl;
+	return 0;
+}
